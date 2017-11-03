cd999ec84fa456fac9e8fb68e5681d66f1e6512c
jan-matthis <mail@jan-matthis.de>
2017-11-03 21:19:55 +0100

    squeeze extra dim

--- delfi/neuralnet/NeuralNet.py
+++ delfi/neuralnet/NeuralNet.py
@@ -192,8 +192,8 @@ class NeuralNet(object):
         self.lprobs_comps = [-0.5 * tt.sum(tt.sum((self.params - m).dimshuffle(
             [0, 'x', 1]) * U, axis=2)**2, axis=1) + ldetU
             for m, U, ldetU in zip(self.ms, self.Us, self.ldetUs)]
-        self.lprobs = MyLogSumExp(tt.stack(self.lprobs_comps, axis=1) + tt.log(self.a), axis=1) \
-                      - (0.5 * self.n_outputs * np.log(2 * np.pi)).squeeze()
+        self.lprobs = (MyLogSumExp(tt.stack(self.lprobs_comps, axis=1) + tt.log(self.a), axis=1) \
+                      - (0.5 * self.n_outputs * np.log(2 * np.pi))).squeeze()
 
         # the quantities from above again, but with deterministic=True
         # --- in the svi case, this will disable injection of randomness;
@@ -209,8 +209,8 @@ class NeuralNet(object):
         self.dlprobs_comps = [-0.5 * tt.sum(tt.sum((self.params - m).dimshuffle(
             [0, 'x', 1]) * U, axis=2)**2, axis=1) + ldetU
             for m, U, ldetU in zip(self.dms, self.dUs, self.dldetUs)]
-        self.dlprobs = MyLogSumExp(tt.stack(self.dlprobs_comps, axis=1) + tt.log(self.da), axis=1) \
-                       - (0.5 * self.n_outputs * np.log(2 * np.pi)).squeeze()
+        self.dlprobs = (MyLogSumExp(tt.stack(self.dlprobs_comps, axis=1) + tt.log(self.da), axis=1) \
+                       - (0.5 * self.n_outputs * np.log(2 * np.pi))).squeeze()
 
         # parameters of network
         self.aps = ll.get_all_params(last_mog)  # all parameters
